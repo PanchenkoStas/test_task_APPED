330059a4a634684206ef32a51ec797553c80e1ad
PanchenkoStas <panchenko.stas333@gmail.com>
2021-11-11 12:51:05 +0200

    change title scc add window scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import { useRouter } from 'next/dist/client/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    // some browsers (like safari) may require a timeout to delay calling this
+    // function after a page has loaded; otherwise, it may not update the position
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return <Component {...pageProps} />
 }
 
